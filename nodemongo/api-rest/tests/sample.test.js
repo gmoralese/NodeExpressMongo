84c9399776f1f37a90fac1f9d8f8a304c67f0fb0
Gustavo <gmoralese@correo.uss.cl>
2020-11-13 17:17:37 -0300

    ✨ improve test

--- nodemongo/api-rest/tests/sample.test.js
+++ nodemongo/api-rest/tests/sample.test.js
@@ -25,9 +25,9 @@ describe('Test User model', () => {
     // Test Schema is working!!!
     // You shouldn't be able to add in any field that isn't defined in the schema
     it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
-        const userWithInvalidField = new UserModel({ username: 'Gustavo' });
+        const userWithInvalidField = new UserModel({ username: 'Gustavo', nickname: 'EL gustavo' });
         const savedUserWithInvalidField = await userWithInvalidField.save();
         expect(savedUserWithInvalidField._id).toBeDefined();
-        expect(savedUserWithInvalidField.nickkname).toBeUndefined();
+        expect(savedUserWithInvalidField.nickname).toBeUndefined();
     });
 })
